07cf723a89768caa72349a064824e0071b47ec82
Matthew Jakeman <mjakeman26@outlook.co.nz>
2021-12-12 14:45:01 +1300

    cursor: Clean up legacy/experimental code

--- src/lsmmathmlcursor.c
+++ src/lsmmathmlcursor.c
@@ -93,25 +93,6 @@ lsm_mathml_cursor_set_property (GObject      *object,
     }
 }
 
-/*static LsmMathmlPosition *
-do_move_recursive (LsmMathmlCursor *self, LsmDirection direction)
-{
-    int new_index = -1;
-    LsmDomNode *element = LSM_DOM_NODE (self->current);
-
-    // TODO: Won't work, as self->position is only valid for current position in tree (i.e. will
-    // be different position for parents)     ...what now?
-    while (!(LSM_MATHML_ELEMENT_CLASS (element)->get_next_position(LSM_MATHML_ELEMENT (element), self->position, direction, &new_index)))
-    {
-        element = lsm_dom_node_get_parent_node (LSM_DOM_NODE (element));
-        if (!element)
-            return NULL;
-    }
-
-    // TODO: Once an element accepts the movement, go to its "first" position
-    return lsm_mathml_position_new (LSM_MATHML_ELEMENT (element), new_index);
-}*/
-
 static void
 get_points_recursive (LsmMathmlElement *element, GList **points)
 {
@@ -160,24 +141,8 @@ lsm_mathml_cursor_get_insertion_points (LsmMathmlElement *root)
 {
     GList *points = NULL;
 
-    // DFS traverse over element
-    // get all insertion points
+    // DFS traverse over element and collect all insertion points
     get_points_recursive (root, &points);
-
-    // later:
-    // loop over insertion points
-    // find closest rectilinear distance
-    /*for (GList *l = points; l != NULL; l = l->next)
-    {
-        LsmMathmlPosition *pos = LSM_MATHML_POSITION (l->data);
-
-        int position;
-        LsmMathmlElement *parent;
-        g_object_get (pos, "parent", &parent, "position", &position, NULL);
-        g_print ("Insertion Point: %s %d\n",
-                 lsm_dom_node_get_node_name (LSM_DOM_NODE (parent)),
-                 position);
-    }*/
 
     return points;
 }
@@ -241,11 +206,7 @@ lsm_mathml_cursor_get_nearest_insertion_point (LsmMathmlElement *root, double x,
         // TODO: Make elements define their own insertion points
         // (otherwise defeats the purpose)
 
-        // TODO: We should find the shortest distance between two
-        // lines. That's a bit hard, so we'll find the midpoint distance
-        // for now.
-
-        double midpoint_x, midpoint_y;
+        double midpoint_x;
 
         if (position == 0)
         {
@@ -258,9 +219,6 @@ lsm_mathml_cursor_get_nearest_insertion_point (LsmMathmlElement *root, double x,
             midpoint_x = parent->x + parent->bbox.width;
         }
 
-        midpoint_y = ((parent->y - parent->bbox.height) + (parent->y + parent->bbox.depth)) / 2;
-
-        // double distance = sqrt(pow(x - midpoint_x, 2) + pow(y - midpoint_y, 2));
         double distance = min_distance_point_to_line (x, y, midpoint_x, parent->y - parent->bbox.height, parent->y + parent->bbox.depth);
 
         if (distance < min_distance)
@@ -341,88 +299,6 @@ lsm_mathml_cursor_move (LsmMathmlCursor *self, LsmMathmlElement *root, LsmDirect
     }
 
     g_object_notify_by_pspec (self, properties[PROP_CURRENT]);
-
-    /*LsmMathmlElement *move_to = NULL;
-    int new_pos = self->position;
-
-    if (!self->current)
-        return;
-
-    /*LsmMathmlPosition *test = do_move_recursive (self, direction);
-    if (test)
-    {
-        int position;
-        LsmMathmlElement *parent;
-        g_object_get (test, "position", &position, "parent", &parent, NULL);
-        g_print ("Result of Move: Element '%s' at index '%d'",
-                 lsm_dom_node_get_node_name (parent), position);
-    }
-
-    if (direction == LSM_DIRECTION_LEFT)
-    {
-        // TODO: First try move within the current element
-
-        if (self->position != 0)
-        {
-            // At end, try to move to a child element
-            move_to = lsm_dom_node_get_last_child (self->current);
-            new_pos = 1;
-
-            if (move_to && LSM_IS_MATHML_ELEMENT (move_to))
-                goto move;
-        }
-
-        // Failing that, move to the previous sibling node
-        move_to = lsm_dom_node_get_previous_sibling (self->current);
-        new_pos = 1;
-
-        if (move_to && LSM_IS_MATHML_ELEMENT (move_to))
-            goto move;
-
-        // Move to parent start
-        move_to = lsm_dom_node_get_parent_node (self->current);
-        new_pos = 0;
-
-        if (move_to && LSM_IS_MATHML_ELEMENT (move_to))
-            goto move;
-    }
-    else if (direction == LSM_DIRECTION_RIGHT)
-    {
-        // Try move within the current element
-        // TODO HERE
-
-        if (self->position != 1)
-        {
-            // At start, try to move to a child element
-            move_to = lsm_dom_node_get_first_child (self->current);
-            new_pos = 0;
-
-            if (move_to && LSM_IS_MATHML_ELEMENT (move_to))
-                goto move;
-        }
-
-        // Try move to next sibling
-        move_to = lsm_dom_node_get_next_sibling (self->current);
-        new_pos = 0;
-
-        if (move_to && LSM_IS_MATHML_ELEMENT (move_to))
-            goto move;
-
-        // Failing that, move to parent end
-        move_to = lsm_dom_node_get_parent_node (self->current);
-        new_pos = 1;
-
-        if (move_to && LSM_IS_MATHML_ELEMENT (move_to))
-            goto move;
-    }
-
-    return;
-
-move:
-    // TODO: Emit notify?
-    self->current = move_to;
-    self->position = new_pos;
-    g_print ("Moved to: %s (%d)\n", lsm_dom_node_get_node_name (self->current), self->position);*/
 }
 
 static void
